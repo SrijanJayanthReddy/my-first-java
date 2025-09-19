c6c68c6324dd3ac5236fbf503d6dfd70367708c0
SANDI <srijanreddysandi1234@gmail.com>
2025-09-19 17:46:18 +0530

    Update UserServlet.java

--- src/main/java/com/example/UserServlet.java
+++ src/main/java/com/example/UserServlet.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class UserServlet extends HttpServlet {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/srijan";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "srijan";
+    private static final String JDBC_PASSWORD = "Srijan@2003";
 
     // Method to display users
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
